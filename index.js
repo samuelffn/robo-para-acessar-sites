c57946452a76cd67b054bda067e35d93f8f66375
samuelffn <samuelffn@gmail.com>
2020-05-31 11:37:47 -0300

    Downgrade na versão do pupperteer e adicionar bloca try/catch no código

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const puppeteer = require('puppeteer');
 console.log('Bem vindo ao conversor');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  try{
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({path: 'example.png'});
 
-  await browser.close();
+    await browser.close();
+  }
+  catch(e){
+      console.log(e);
+  }
 })();
